dc7b6133c209dd4f86f8311e11447bc147437075
aa <aa@163.com>
2023-05-15 09:47:08 +0800

    整理目录

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# qy-koolshare
-koolshare qy acc
+# Alist文件列表
+
+## 是什么？
+`支持PS、Switch、Xbox等主机加速`
+
+
+## 项目地址
+https://github.com/qiyouacc/qy-koolshare
+
+## 官方信息  
+
+www.qiyou.cn
---
+++ config.json.js
@@ -0,0 +1,11 @@
+{
+    "author": "成都俊云科技有限公司",
+    "description": "支持PS、Switch、Xbox等主机加速",
+    "home_url": "Module_qyacc.asp",
+    "link": "https://github.com/qiyouacc/qy-koolshare",
+    "md5": "d88d5c6b28edb836d501e06c8d8fc006", 
+    "module": "qyacc",
+    "tags": "主机加速",
+    "title": "奇游联机宝插件",
+    "version": "1.0.1"
+}
